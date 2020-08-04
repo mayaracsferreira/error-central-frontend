cef1cbef5a2cf3b79c59303950ddf85a4e278f78
Mayara Cristina da Silva Ferreira <mcristina@img.com.br>
2020-08-04 00:55:46 -0300

    ajuste de diretorio no server.js

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/error-central-frontend'));
+app.use(express.static(__dirname + '/error-central-frontend/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/error-central-frontend/index.html'));
+app.get('/*', function(req,res) {    
+res.sendFile(path.join(__dirname+'/error-central-frontend/dist/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
+
+
